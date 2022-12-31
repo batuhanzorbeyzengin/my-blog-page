03ca43996a1eb11e24815e93cb935a812d576b64
Batuhan Zorbey Zengin <batuhanzorbeyzengin@gmail.com>
2022-12-31 19:43:49 +0300

    dev

---
+++ components/HomeBlogSection/index.js
@@ -0,0 +1,12 @@
+import styles from '../HomeBlogSection'
+
+
+export default function HomeBlogSection() {
+    return(
+        <section id='blog_list' className='row'>
+            <div>
+
+            </div>
+        </section>
+    )
+}
